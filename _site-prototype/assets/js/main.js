d80bce359d96b7bca3a5d0a18c4ba22bf64aa4ca
egladman <eli.gladman@gmail.com>
2015-04-22 18:07:58 -0400

    added responsive navbar

---
+++ _site-prototype/assets/js/main.js
@@ -0,0 +1,11 @@
+var  mn = $(".main-nav");
+    mns = "main-nav-scrolled";
+    hdr = $('header').height();
+
+$(window).scroll(function() {
+  if( $(this).scrollTop() > hdr ) {
+    mn.addClass(mns);
+  } else {
+    mn.removeClass(mns);
+  }
+});
