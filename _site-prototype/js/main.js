6f7b194c96f9239525500d6ac677b949077b1f4a
egladman <eli.gladman@gmail.com>
2015-04-23 18:42:10 -0400

    bedazzlement

---
+++ _site-prototype/js/main.js
@@ -0,0 +1,15 @@
+jQuery(document).ready( function(){
+
+  $('#brand').removeClass('hidden');
+  $('#brand').addClass('animated zoomInDown');
+
+  $('#button, #heading').removeClass('hidden');
+  $('#button, #heading').addClass('animated zoomIn');
+
+  $('#main-nav').removeClass('hidden');
+  $('#main-nav').addClass('animated fadeIn');
+
+  $('#scroll-indicator').removeClass('hidden');
+  $('#scroll-indicator').addClass('animated bounce');
+
+});
