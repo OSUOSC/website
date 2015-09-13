1cd63f7978f81f612ea8c1ee21d3077c00bf354d
Eli Gladman <eli.gladman@gmail.com>
2015-09-13 17:43:05 -0400

    fixed various bugs

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -55,6 +55,30 @@ function setNavigation() {
 
 
 
+
+
+
+function moveClouds(e, s, d) {
+    $(e).css('right', '-20%');
+        var wait = window.setTimeout(function(){
+            $(e).animate ({
+                right: '120%'
+            }, s, 'linear', function() {
+            moveClouds(e, s, d);
+        });
+    },d);
+}
+// if(!Modernizr.cssanimations) {
+//     var clouds = [1,2,3,4];
+//
+//     $.each(clouds, function() {
+//         var e = $('.cloud-' + this);
+//         moveClouds(e, e.data('speed'), e.data('delay'));
+//     });
+// }
+
+
+
 // ICON SHADOWS
 
 ! function(e) {
