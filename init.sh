7c274707e9702eeffe227d69657ad2824d13684b
Eli Gladman <eli.gladman@gmail.com>
2015-10-30 16:33:27 -0400

    bash script to setup dev

---
+++ init.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+# TODO
+# add error handling
+
+# install grunt-cli globally
+npm install -g grunt-cli
+
+# install remaining modules relative to the user
+npm install
+
+# install bundler gem
+gem install bundler
