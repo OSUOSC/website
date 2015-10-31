ffff00399103e9e41252d35c50de2c1e9e5945e2
Eli Gladman <eli.gladman@gmail.com>
2015-10-31 17:18:13 -0400

    added error handling to init script

--- init.sh
+++ init.sh
@@ -1,13 +1,47 @@
 #!/bin/bash
 
-# TODO
-# add error handling
+NODE_VERSION=0.12.7
+RUBY_VERSION=2.2.2
 
-# install grunt-cli globally
-npm install -g grunt-cli
 
-# install remaining modules relative to the user
+echo "Checking for Node version ${NODE_VERSION}"
+node -v | grep ${NODE_VERSION} >/dev/null 2>&1
+if [[ $? != 0 ]]; then
+    echo -n "Node version ${NODE_VERSION} is not installed"
+    exit
+fi
+
+
+echo "Checking for Ruby version ${RUBY_VERSION}"
+ruby -v | grep ${RUBY_VERSION} >/dev/null 2>&1
+if [[ $? != 0 ]]; then
+    echo -n "Ruby version ${RUBY_VERSION} is not installed"
+    exit
+fi
+
+
+
+p=$(eval npm list -g | grep grunt-cli)
+
+echo "Checking for grunt-cli"
+npm -v grunt-cli >/dev/null 2>&1
+if [[ $? != 0 ]]; then
+    echo -n "grunt-cli is not installed"
+elif [[ ${p//[^0-9_.]/} = 0 ]]; then
+  echo "grunt-cli isn't installed globally"
+
+  npm install -g grunt-cli
+fi
+
+
+#TODO
+# check if package dependencies are indeed installed
+
+# install remaining modules locally
 npm install
 
-# install bundler gem
-gem install bundler
+
+echo 'Checking for bundler gem'
+if ! [ $(gem list bundler -i) ]; then
+  gem install bundler
+fi
