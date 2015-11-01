65a65a6adabb4706683b5e407e034f77fdf2e274
Eli Gladman <eli.gladman@gmail.com>
2015-11-01 18:26:22 -0500

    added js to intialize search

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -24,6 +24,15 @@ $(document).ready(function() {
 $(function() {
     setNavigation();
     setWidth();
+    SimpleJekyllSearch({
+     searchInput: document.getElementById('search-input'),
+     resultsContainer: document.getElementById('search-results'),
+     json: '/search.json',
+     searchResultTemplate: '<li><a href="{url}" title="{desc}">{title}</a></li>',
+     noResultsText: '<li>No results found</li>',
+     limit: 10,
+     fuzzy: false,
+    })
 });
 
 $(window).resize(function() {
